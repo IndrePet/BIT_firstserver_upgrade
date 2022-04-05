5f465138801c17c7398aee33a6618c70ab2b649a
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-04-05 22:10:19 +0300

    Tests for frontend validation class methods

--- public/js/pages/auth.js
+++ public/js/pages/auth.js
@@ -1,3 +1,5 @@
+import { IsValid } from "../components/is-valid/IsValid.js";
+
 /*
 1) susirasti forma ir jos VISUS laukus
 2) surinkti informacija is formos
@@ -9,19 +11,33 @@
 6b) jei serveris ne rado klaidu - END
 */
 
-const formDOM = document.querySelector('.form');
-const errorsDOM = formDOM.querySelector('.form-errors');
-const allInputsDOM = formDOM.querySelectorAll('input');
-const submitDOM = formDOM.querySelector('button');
+const formDOM = document.querySelector(".form");
+const errorsDOM = formDOM.querySelector(".form-errors");
+const allInputsDOM = formDOM.querySelectorAll("input");
+const submitDOM = formDOM.querySelector("button");
 
-submitDOM.addEventListener('click', (e) => {
+submitDOM.addEventListener("click", (e) => {
   e.preventDefault();
 
+  const errors = [];
   const formData = {};
   for (const inputDOM of allInputsDOM) {
-    const { id, value } = inputDOM;
+    const { id, value, dataset } = inputDOM;
+    const validationRule = IsValid[dataset.validation];
+    const [err, status] = validationRule(value);
+    if (err) {
+      errors.push(status);
+    }
     formData[id] = value;
   }
 
-  console.log(formData);
+  if (formData.password !== formData.repass) {
+    errors.push("Nesutampa slaptazodziai");
+  }
+  errorsDOM.innerText = errors.join("\r\n");
+
+  if (errors.length === 0) {
+    delete formData.repass;
+    console.log("SIUNCIAME I SERVERI:", formData);
+  }
 });
